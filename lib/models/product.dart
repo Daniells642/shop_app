be9a6a6b7cde6c5dbab710d0c73734e72b2b5f1b
Daniel Lima Sousa <109328889+Daniells642@users.noreply.github.com>
2025-01-12 23:25:08 -0300

    Estrutura criada da página "main" e dados mocados.

---
+++ lib/models/product.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+//método para alterar se o produto é favorito ou não.
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+  }
+}
+
---
+++ lib/pages/products_overViewPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/data/dummy_products.dart';
+import 'package:shop/componentes/product_item.dart';
+
+class ProductOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Minha Loja"),
+        //backgroundColor: Colors.blue,
+        
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount:
+            loadedProducts.length, //Pega a quantidade de itens dentro da lista.
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        //Define uma estrutura de grid, exibindo 2 elementos na tela.
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
