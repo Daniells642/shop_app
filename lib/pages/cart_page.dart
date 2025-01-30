8ebc3d6f1ab10324db0e890eb29a60762049bdf2
Daniel Lima Sousa <109328889+Daniells642@users.noreply.github.com>
2025-01-29 23:43:42 -0300

    mostando o valor total dos itens na tela cart_page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-   
+    final Cart cart = Provider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                   Chip(
+                    backgroundColor: Colors.purple,
+                    label: Text(
+                      'R\$${cart.totalAmount}',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        textStyle: const TextStyle(color: Colors.purple)),
+                    onPressed: null,
+                    child: const Text("COMPRAR"),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
