1a64b8535368701b7711c89ffc39c569b1a82ddf
Daniel Lima Sousa <109328889+Daniells642@users.noreply.github.com>
2025-01-30 00:18:50 -0300

    Excluir os item dentro da tela cart_page, com a propriedade Dismissible

--- lib/componentes/cart_item.dart
+++ lib/componentes/cart_item.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/cart.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
   final CartItem cartItem;
 
-
   const CartItemWidget({super.key, required this.cartItem});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 5,
-      ),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-          child: FittedBox(
-            child: Text('${cartItem.price}'),
-          ),),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
         ),
-        title: Text(cartItem.name),
-        subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
-        trailing: Text('${cartItem.quantity}X'),
+        child: const Icon(Icons.delete, color: Colors.white, size: 40),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: FittedBox(
+                child: Text('${cartItem.price}'),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}X'),
+        ),
       ),
     );
   }
