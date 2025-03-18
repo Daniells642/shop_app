d2e57e7977be33bac834fac4f53164f4850784d9
Daniel Lima Sousa <109328889+Daniells642@users.noreply.github.com>
2025-03-18 01:13:39 -0300

    Enviando os pedidos para o banco de dados.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -43,19 +43,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(
-                    // style: TextButton.styleFrom(
-                    //     textStyle: const TextStyle(color: Colors.purple)),
-                    onPressed: () {
-                      Provider.of<OrderList>(
-                        context,
-                        listen: false,
-                      ).addOrder(cart);
-
-                      cart.clear();
-                    },
-                    child: const Text("COMPRAR"),
-                  )
+                  CartButton(cart: cart)
                 ],
               ),
             ),
@@ -71,5 +59,46 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  final bool _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            // style: TextButton.styleFrom(
+            //     textStyle: const TextStyle(color: Colors.purple)),
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() {
+                      _isLoading == true;
+                    });
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
+                    setState(() {
+                      _isLoading == false;
+                    });
+                    widget.cart.clear();
+                  },
+            child: const Text("COMPRAR"),
+          );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
+  // ignore: non_constant_identifier_names
   static String PRODUCT_BASE_URL = "https://shop-teste-ddc6e-default-rtdb.firebaseio.com/products";
+  // ignore: non_constant_identifier_names
+  static String ORDER_BASE_URL = "https://shop-teste-ddc6e-default-rtdb.firebaseio.com/orders";
 }
